8dc6b1aeab53f5f23c349514e4e1d6fa3b803ee4
para7 <cometpara7@gmail.com>
2025-12-01 01:47:26 +0900

    refactor

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,26 @@
 import { showRoutes } from "hono/dev";
 import { createApp } from "honox/server";
-import { setup } from "./middlewares";
+import { createMiddleware } from "hono/factory";
+import { getDb } from "./lib/db";
+import { repositories } from "./repositories";
+import { usecases } from "./usecases";
+
+// 依存性注入ミドルウェア
+export const setup = createMiddleware(async (c, next) => {
+	// DB接続
+	const db = getDb(c);
+
+	// Repositories生成
+	const repos = repositories(db);
+
+	// Usecases生成
+	const cases = usecases(repos);
+
+	// Contextに設定（usecasesのみ）
+	c.set("usecases", cases);
+
+	await next();
+});
 
 const app = createApp({
 	init(app) {
